8389be2c0feda949edfefa9e8d2fa09b3428f5d7
scld <scld@leeds.ac.uk>
2018-08-01 11:40:11 +0100

    Added nonhomogeneous poisson

--- population_density/current_based_synapse/nonhomogeneous_poisson.py
+++ population_density/current_based_synapse/nonhomogeneous_poisson.py
@@ -1,51 +1,30 @@
 import numpy as np
-from scipy.optimize import root 
 import matplotlib.pyplot as plt
 from scipy.misc import factorial
+from scipy.optimize import root 
+from scipy.integrate import quad
+from scipy.stats import poisson
 
-
-def generate_nonhomogeneous_poisson_0(r, T, r0):
+def generate_nonhomogeneous_poisson_0(r, T, r_max):
+    '''Generate spike train from nonhomogeneous Poisson process with rate function r_t within recording time T
+    param r: rate function
+    param T: recording time
+    param r_max: max(r) for t in [0,T]
+    return spike_times: array with spike times'''
     
-    N = np.random.poisson(r0*T)
+    N = np.random.poisson(r_max*T)
     
-    spike_times = np.sort(T*np.random.uniform(size = N))
+    # draw spike for homogeneous Poisson process with rate r_max
+    hom_spike_times = np.sort(np.random.uniform(0, T, N)) 
 
     #thinning
+    p = r(hom_spike_times)/r_max # probability to accept spike
+    u = np.random.uniform(size = N)        
     
-    u = np.random.uniform(size = N)
+    spike_times = hom_spike_times[u <= p]
     
-    spike_times_0 = []
+    return spike_times
     
-    for i, t in enumerate(spike_times):
-        
-        p = 1. - r(t)/r0
-        if u[i] > p:
-            spike_times_0.append(t)
-    
-    return np.array(spike_times_0)
-    
-def generate_nonhomogeneous_poisson_1(r_t, T):
-    '''Generate spike train from nonhomogeneous Poisson process with rate function r_t for a recording time T
-    param r_t: rate function
-    param T: recording time
-    return spike_times: array with spike times:'''
-
-    t = 0 # initial time
-    
-    spike_times = []
-            
-    while t<T:
-        
-        # uniform random number
-        u = np.random.uniform()
-        
-        # invert cdf to draw ISI 
-        isi = root(lambda y: 1. - np.exp(-r_t(t+y) + r_t(t)) - u, 1./r_t(t)).x 
-        t = isi + t # new spike time previous plus ISI
-        spike_times.append(t)
-    
-    return np.array(spike_times)
-
 
 #------------------------------------------------------------------------------ 
 # parameter
@@ -56,51 +35,68 @@ tau = 1. # membrane time constant
 T = 10.*tau # choose period large compared to the membrane time constannt
 w = 2.*np.pi/T
 
-A  = 2. # amplitude of oscillations 
-r0 = 5.# stationary rate 
+A  = 5. # amplitude of oscillations 
+r0 = 20. # stationary rate 
+r_max = r0 + A
 
 r = lambda t: A*np.sin(w*t) + r0 # rate function
 
-Tr = 10000*T # recording time
+Tr = 2*T # recording time
 
 #------------------------------------------------------------------------------ 
-# generate spike trains and check statistics  
+# generate ensemble of spike trains and check statistics  
 
-spike_train = generate_nonhomogeneous_poisson_0(r, Tr, r0 + A)
+N = 100000 # number of trials
 
-t0 = 0
-t1  = 0.5*T
+# select random time window
 
-N_arr = []
+t0 = np.random.uniform(0, Tr)
+t1 = np.random.uniform(0, Tr)
 
-while t1 <= Tr:
-                        
-    idx = np.logical_and(t0 < spike_train, spike_train <= t1)
+if t0 < t1:
+    pass
+else:
+    t = t0
+    t0 = t1
+    t1 = t
     
-    N_arr.append(len(spike_train[idx]))
+N_arr = np.zeros(N) 
+ 
+for i in xrange(N):
+
+    spike_train = generate_nonhomogeneous_poisson_0(r, Tr, r0 + A)
     
-    t0 = t0 + T
-    t1 = t1 + T 
+    hits = np.logical_and(t0 <= spike_train, spike_train <= t1)
+    count = np.sum(hits)
+    N_arr[i] = count
+    
+N_min = min(N_arr)
+N_max = max(N_arr)
+
+n_arr = np.arange(N_min, N_max)
+
+p_arr_0 = np.zeros_like(n_arr)
+
+# theoretical prediction for spike count statistics 
+lam = quad(r, t0, t1)[0] # rate integral 
+p_arr_1 = np.zeros_like(n_arr)
+
+for i,n in enumerate(n_arr):
+    
+    count = np.sum(N_arr == n)
+    p = float(count)/N
+    p_arr_0[i] = p
+    # calculated expected prop. from Poisson distribution
+    p_arr_1[i] = poisson.pmf(n, lam)
+    
+#------------------------------------------------------------------------------ 
+# plotting 
+
+gs = plt.GridSpec(1,1)
 
 ax0 = plt.subplot(gs[0])
-
-t = np.linspace(0, Tr, 100*Tr)
-    
-# ax0.hist(spike_train, 60)
-ax0.plot(t, r(t), '-', color = 'red')
-ax0.plot(spike_train, r0*np.ones_like(spike_train), 'o', color = 'blue', markersize = 0.5)
-
-ax1 = plt.subplot(gs[1])
-
-lam = A/w+r0*0.5*T
-
-bins = np.arange(0.5, 5*lam, 1)
-ax1.hist(N_arr, bins, density = True)
-
-n = np.arange(0, round(5*lam), 1)
-y = lam**(n)/factorial(n)*np.exp(-lam)
-
-ax1.plot(n, y)
+ax0.bar(n_arr, p_arr_1, align = 'center', color = 'blue')
+ax0.plot(n_arr, p_arr_0, 'x', color = 'red')
 
 plt.show()
 
